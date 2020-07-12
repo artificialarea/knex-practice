68594861489c41345e8e4bab9665cf08166ac6e0
Sacha Sedriks <sachased@gmail.com>
2020-07-12 13:46:53 -0700

    to use Knex, installed library and a Postgres database driver(pg).

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,8 @@
-console.log('hello, practice')
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL,
+})
+
+console.log('knex and driver installed correctly')
