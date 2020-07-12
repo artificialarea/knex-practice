db41d91c7ed01b7123eaad34dbc4bfce6ad2b6ed
Sacha Sedriks <sachased@gmail.com>
2020-07-12 14:45:52 -0700

    updated to build Knex queries.

--- src/practice.js
+++ src/practice.js
@@ -6,3 +6,16 @@ const knexInstance = knex({
 })
 
 console.log('knex and driver installed correctly')
+
+// Knex query syntax variants
+// equivalent to SQL SELECT query
+// const q1 = knexInstance('amazong_products').select('*').toQuery()
+// const q2 = knexInstance.from('amazong_products').select('*').toQuery()
+// console.log('q1: ', q1)
+// console.log('q2: ', q2)
+
+// Note: this is the first point in time that our Node script attempts to connect to the database. You may see errors if the connection string is incorrect, if the database doesn't exist, or if the table doesn't exist.
+knexInstance('amazong_products').select('*')
+    .then(result => {
+        console.log(result)
+    })
