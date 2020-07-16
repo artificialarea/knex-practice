00b578789c144a0477b5ba3d9ab7c0252923ae87
Sacha Sedriks <sachased@gmail.com>
2020-07-15 21:57:06 -0700

    added ArticleService object tests. Required injecting Knex db instance and before/after mocha hooks.

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,7 +1,12 @@
 const ArticlesService = {
 
-    getAllArticles() {
-        return 'all the articles...'
+    // knex db is being injected into this fn as a param
+    getAllArticles(knex) { 
+        
+        return knex.select('*').from('blogful_articles')
+
+        // return Promise.resolve('all the articles!!')
+        // The getAllArticles will use asynchronous logic for the SQL query so our test code will assume getAllArticles returns a promise that resolves the articles.
     }
 
 }
